c7b0e29abef7a76bad3439355a278f4c39744d7c
Suganya Sivaskantharajah <suganya.sivaskantharajah@digital.cabinet-office.gov.uk>
2019-05-09 12:21:13 +0100

    test word2vec embeddings
    more logging stuff

--- src/models/train_node2vec_model.py
+++ src/models/train_node2vec_model.py
@@ -12,25 +12,25 @@ logging.config.fileConfig('src/logging.conf')
 
 def train_node2_vec_model(edges_df, node_id_content_id_mapping,
                           word2vec_workers=3):
-    train_node2_vec_model_logger = logging.getLogger('train_node2_vec_model.train_node2_vec_model')
+    logger = logging.getLogger('train_node2_vec_model.train_node2_vec_model')
 
-    train_node2_vec_model_logger.info('creating graph from edges_df')
+    logger.info('creating graph from edges_df')
     graph = nx.convert_matrix.from_pandas_edgelist(
         edges_df, source='source', target='target'
         # when we have functional network too, we can add weight
         # , edge_attr='weight'
     )
 
-    train_node2_vec_model_logger.info('adding attributes to graph')
+    logger.info('adding attributes to graph')
     attributes = {node_id: {"content_id": node_id_content_id_mapping[node_id]}
                   for node_id in graph.nodes()}
     nx.set_node_attributes(graph, attributes)
 
-    train_node2_vec_model_logger.info('Precomputing probabilities and generating walks')
+    logger.info('Precomputing probabilities and generating walks')
     node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=300,
                         workers=1)
 
-    train_node2_vec_model_logger.info('Fit node2vec model (create embeddings for nodes)')
+    logger.info('Fit node2vec model (create embeddings for nodes)')
     # Any keywords acceptable by gensim.Word2Vec can be passed, `dimensions` and `workers` are
     # automatically passed (from the Node2Vec constructor)
     # https://radimrehurek.com/gensim/models/word2vec.html#gensim.models.word2vec.Word2Vec
@@ -41,8 +41,9 @@ def train_node2_vec_model(edges_df, node_id_content_id_mapping,
 
 if __name__ == "__main__":  # our module is being executed as a program
     datadir = os.getenv("DATADIR")
-    logger = logging.getLogger('train_node2_vec_model')
+    module_logger = logging.getLogger('train_node2_vec_model')
 
+    module_logger.info(f'reading in structural_network.csv and node_id_content_id_mapping.json')
     edges = pd.read_csv(os.path.join(
         datadir, 'tmp', 'structural_network.csv'))
     with open(
@@ -60,10 +61,10 @@ if __name__ == "__main__":  # our module is being executed as a program
 
     node_embeddings_file_path = os.path.join(save_models_dir,
                                              "n2v_node_embeddings")
-    logger.info(f'saving node embeddings to {node_embeddings_file_path}')
+    module_logger.info(f'saving node embeddings to {node_embeddings_file_path}')
     node2vec_model.wv.save_word2vec_format(node_embeddings_file_path)
 
     node2vec_model_file_path = os.path.join(save_models_dir, "n2v.model")
-    logger.info(f'saving model to {node2vec_model_file_path}')
+    module_logger.info(f'saving model to {node2vec_model_file_path}')
     node2vec_model.save(node2vec_model_file_path)
     # should we test saving and loading models and embeddings?
